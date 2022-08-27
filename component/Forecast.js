13e470e5942eca5781dea063864f9c15491e873d
Grirkrit phiphatsombat <nut220445@gmail.com>
2022-08-27 22:45:19 +0700

    part 6 commit 3

--- component/Forecast.js
+++ component/Forecast.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, Image } from 'react-native'
 
 export default function Forecast(props) {
     return (
+        
         <View >
-            <Text style={style.normalText}>{props.line}</Text>
-            <Text style={style.Text}>main</Text>
+            <Image source={{uri: `https://openweathermap.org/img/wn/${props.icon}@2x.png`}}
+                style={{width: 100, height: 100}}/>
+            <Text>{props.line}</Text>
             <Text style={style.normalText}>{props.main}</Text>
-            <Text style={style.normalText}>description</Text>
+            <Text>{props.line}</Text>
             <Text style={style.normalText}>{props.description}</Text>
             <View style= {{
                 flexDirection: "row",
                 justifyContent: 'center',
             }}></View>
+            
             <Text style={style.Text}>{props.temp} <Text style={style.normalText}>°C</Text></Text>
         </View>
     );
@@ -25,16 +28,17 @@ const style = StyleSheet.create(
             fontSize: 20,
             fontWeight: "bold",
             color: 'white',
+            fontWeight: '300'
 
         },
 
         Text: {
-            fontSize: 40, 
+            fontSize: 35, 
             fontWeight: "bold", 
             color: 'white', 
             textAlign: 'center',
-            lineHeight: 60
-
-        }
+            lineHeight: 60,
+            fontWeight: '400'
+        },
     }
-)
+) 
--- component/Weather.js
+++ component/Weather.js
@@ -8,13 +8,14 @@ export default function Weather(props) {
     useEffect(() => {
         console.log(`fetching data with zipCode = ${props.zipCode}`)
         if (props.zipCode) {
-            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=89b6cc5abffc11c37c735486e081fc0f`)
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=0bf83bc8cae3ec34c7e1a057b93deacf`)
                 .then((response) => response.json())
                 .then((json) => {
                     setForecastInfo({
                         main: json.weather[0].main,
                         description: json.weather[0].description,
-                        temp: json.main.temp
+                        temp: json.main.temp,
+                        icon: json.weather[0].icon
                     });
             })
             .catch((error) => {
@@ -46,6 +47,7 @@ export default function Weather(props) {
                     <Text style={styles.titleText}>Zip code is {props.zipCode}.</Text>
                 </View>
             </View>
+            
             <View>
                 <Forecast {...forecastInfo}/>
             </View>
@@ -96,10 +98,4 @@ const styles = StyleSheet.create({
         marginTop: 10
     },
 
-    am: {
-        fontSize: 30,
-        color: 'white',
-        fontWeight: '300',
-        textAlign: 'center'
-    }
 });
