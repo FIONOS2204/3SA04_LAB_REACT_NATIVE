466e03abb22a59b60b98b94bd20a98b097747d46
Grirkrit phiphatsombat <nut220445@gmail.com>
2022-08-27 23:31:27 +0700

    part 6 commit 5

--- component/screen/WeatherScreen.js
+++ component/screen/WeatherScreen.js
@@ -2,11 +2,12 @@ import React from "react";
 import { StatusBar, View } from "react-native";
 import Weather from '../components/Weather'
 
+
 export default function WeatherScreen({route}) { 
     return (
         <View>
-            <Weather zipCode={route.params.zipCode}/>
-            <StatusBar style="auto"/>
+            <Weather zipCode={route.params.zipCode} place={route.params.place} />
+            <StatusBar style="auto" />
         </View>
     );
 }
