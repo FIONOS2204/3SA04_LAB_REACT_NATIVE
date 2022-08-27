61cb54d63fb7970c3124da9ab4b3d9fcef5e50b8
Grirkrit phiphatsombat <nut220445@gmail.com>
2022-08-27 11:46:15 +0700

    part 4

--- component/Weather.js
+++ component/Weather.js
@@ -1,30 +1,46 @@
 import React from "react";
 import { Text, ImageBackground, StyleSheet } from "react-native"
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Forecast from './Forecast';
 
 export default function Weather(props){
+        useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=89b6cc5abffc11c37c735486e081fc0f`)
+            .then((response) => response.json())
+            .then((json) => {
+                setForecastInfo({
+                    main: json.weather[0].main,
+                    description: json.weather[0].description,
+                    temp: json.main.temp
+                });
+            })
+        .catch((error) => {
+            console.warn(error);
+        });
+ }
+ }, [props.zipCode])
 
     const [forecastInfo, setForecastInfo] = useState({
         main: "-",
-        description: "-",
-        temp: 0
-    })
-
-    return (
-           <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
-                <Text>Zip code</Text>
-                <Text>{props.zipCode}</Text>
-                <Forecast {...forecastInfo}/>
-            </ImageBackground> 
+        description: '-',
+temp: 0
+})
+return (<View>
+    <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
+    <Text>Zip Code</Text>
+    <Text>{props.zipCode}</Text>
+    <Forecast {...forecastInfo} />
+    </ImageBackground>
+    </View>
     );
-}
-
-const styles = StyleSheet.create({
+    }
+    const styles = StyleSheet.create({
     backdrop: {
-        alignItems: "center",
-        width: "100%",
-        height: "100%"
+    alignItems: 'center',
+    width: '100%',
+    height: '100%'
     },
-});
+});     
