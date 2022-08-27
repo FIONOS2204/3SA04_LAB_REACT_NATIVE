47dc9c613b986657f53876be31ba49908fa8e633
Grirkrit phiphatsombat <nut220445@gmail.com>
2022-08-27 17:34:51 +0700

    part 5

--- component/Forecast.js
+++ component/Forecast.js
@@ -1,17 +1,40 @@
 import React from "react";
-import { View, Text } from "react-native"
+import { Text, View, StyleSheet } from 'react-native'
 
 export default function Forecast(props) {
- return (
-    <View >
-        <Text>{props.main}</Text>
-        <Text>{props.description}</Text>
-
-        <View>
-            <Text>{props.temp}</Text>
-            <Text>°C</Text>
+    return (
+        <View >
+            <Text style={style.normalText}>{props.line}</Text>
+            <Text style={style.Text}>main</Text>
+            <Text style={style.normalText}>{props.main}</Text>
+            <Text style={style.normalText}>description</Text>
+            <Text style={style.normalText}>{props.description}</Text>
+            <View style= {{
+                flexDirection: "row",
+                justifyContent: 'center',
+            }}></View>
+            <Text style={style.Text}>{props.temp} <Text style={style.normalText}>°C</Text></Text>
         </View>
-
-    </View>
- );
+    );
 }
+
+const style = StyleSheet.create(
+    {
+        normalText: {
+            textAlign: 'center',
+            fontSize: 20,
+            fontWeight: "bold",
+            color: 'white',
+
+        },
+
+        Text: {
+            fontSize: 40, 
+            fontWeight: "bold", 
+            color: 'white', 
+            textAlign: 'center',
+            lineHeight: 60
+
+        }
+    }
+)
--- component/Weather.js
+++ component/Weather.js
@@ -1,46 +1,64 @@
-import React from "react";
-import { Text, ImageBackground, StyleSheet } from "react-native"
-import { useState, useEffect } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { Text, View, ImageBackground, StyleSheet } from 'react-native'
 import Forecast from './Forecast';
+import Constants from 'expo-constants';
 
-export default function Weather(props){
-        useEffect(() => {
+export default function Weather(props) {
+    useEffect(() => {
         console.log(`fetching data with zipCode = ${props.zipCode}`)
         if (props.zipCode) {
             fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=89b6cc5abffc11c37c735486e081fc0f`)
-            .then((response) => response.json())
-            .then((json) => {
-                setForecastInfo({
-                    main: json.weather[0].main,
-                    description: json.weather[0].description,
-                    temp: json.main.temp
-                });
+                .then((response) => response.json())
+                .then((json) => {
+                    setForecastInfo({
+                        main: json.weather[0].main,
+                        description: json.weather[0].description,
+                        temp: json.main.temp
+                    });
             })
-        .catch((error) => {
-            console.warn(error);
-        });
- }
- }, [props.zipCode])
+            .catch((error) => {
+                console.warn(error);
+            });
+        }
+    }, [props.zipCode])
 
     const [forecastInfo, setForecastInfo] = useState({
-        main: "-",
-        description: '-',
-temp: 0
-})
-return (<View>
-    <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
-    <Text>Zip Code</Text>
-    <Text>{props.zipCode}</Text>
-    <Forecast {...forecastInfo} />
-    </ImageBackground>
-    </View>
+        line: '',
+        main: '',
+        description: '',
+        temp: 0
+    })
+
+    return (
+        <View>
+            <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
+                <View style={styles.highlight}>
+                    <Text style={styles.titleText}>Zip code is {props.zipCode}.</Text>
+                    <Forecast {...forecastInfo}/>
+                </View>
+            </ImageBackground>
+        </View>
     );
-    }
-    const styles = StyleSheet.create({
+}
+
+const styles = StyleSheet.create({
     backdrop: {
-    alignItems: 'center',
-    width: '100%',
-    height: '100%'
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
     },
-});     
+    highlight: {
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        width:"100%", 
+        height:"45%", 
+        paddingTop: Constants.statusBarHeight, 
+        alignItems: 'center'
+    },
+
+    titleText: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: 'white',
+        textAlign: 'center'
+    }
+});
