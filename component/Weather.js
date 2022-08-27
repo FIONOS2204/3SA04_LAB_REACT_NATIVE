4a47aa5a6d40dd7790124ff57121b7f3716b0070
Grirkrit phiphatsombat <nut220445@gmail.com>
2022-08-27 20:49:42 +0700

    part 6 commit 1

--- component/Weather.js
+++ component/Weather.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, ImageBackground, StyleSheet } from 'react-native'
+import { Text, View, ImageBackground, StyleSheet, place } from 'react-native'
 import Forecast from './Forecast';
 import Constants from 'expo-constants';
+import DateTime from './DateTime';
 
 export default function Weather(props) {
     useEffect(() => {
@@ -30,14 +31,26 @@ export default function Weather(props) {
     })
 
     return (
-        <View>
-            <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
-                <View style={styles.highlight}>
-                    <Text style={styles.titleText}>Zip code is {props.zipCode}.</Text>
-                    <Forecast {...forecastInfo}/>
+        <ImageBackground source={require('../img.jpg')} style={styles.backdrop}>
+        <View style={styles.highlight}>
+            <View>
+                
+                <View style={styles.container}>
+                    <Text style={styles.place}>{props.place}</Text>
                 </View>
-            </ImageBackground>
+                <View>
+                    <DateTime />
+                </View>
+                <View >
+                    <Text>{props.line}</Text>
+                    <Text style={styles.titleText}>Zip code is {props.zipCode}.</Text>
+                </View>
+            </View>
+            <View>
+                <Forecast {...forecastInfo}/>
+            </View>
         </View>
+        </ImageBackground>
     );
 }
 
@@ -48,9 +61,9 @@ const styles = StyleSheet.create({
         height: '100%'
     },
     highlight: {
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        backgroundColor: 'rgba(0, 0, 0, 0.3)',
         width:"100%", 
-        height:"45%", 
+        height:"100%", 
         paddingTop: Constants.statusBarHeight, 
         alignItems: 'center'
     },
@@ -59,6 +72,34 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: "bold",
         color: 'white',
+        textAlign: 'center',
+        fontWeight: '300'
+    },
+    section: {
+        flex: 1.5,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 20
+    },
+
+    place: {
+        fontSize: 20,
+        color: 'white',
+        fontWeight: '300',
+        textAlign: 'center'
+    },
+
+    container: {
+        backgroundColor: "#18181b99",
+        borderRadius: 10,
+        padding: 10,
+        marginTop: 10
+    },
+
+    am: {
+        fontSize: 30,
+        color: 'white',
+        fontWeight: '300',
         textAlign: 'center'
     }
 });
